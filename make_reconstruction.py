14de0eb1478ec122a0ef83f41ed5a13a4d22d9a4
Jennifer Switzer <jfs@mit.edu>
2021-01-07 19:27:50 -0800

    initial commit

---
+++ make_reconstruction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Image reconstruction in metashape
+"""
+
+import sys
+def main():
+    print('~~~~ reconstruction goes here ~~~~ ')
+    return 0
+
+
+if __name__=='__main__':
+    main()
