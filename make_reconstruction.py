c51f05b3f1eb7f456c7491e07bc15b9bf1b4d460
Jennifer Switzer <jfs@mit.edu>
2021-01-12 14:26:59 -0800

    tell ms to use gpus

--- make_reconstruction.py
+++ make_reconstruction.py
@@ -7,20 +7,26 @@ import glob
 
 def main():
     #make sure that it knows to use gpus
+    Metashape.app.gpu_mask = 2 ** (len(Metashape.app.enumGPUDevices())) - 1
+
+    #initialize
     doc = Metashape.Document()
     chunk = doc.addChunk()
+
+    #add photos
     photos = glob.glob('/extvol/images/*.png')
     chunk.addPhotos(photos)
-    #todo not sure if reference preselection should be true or not
+    
+    #//todo not sure if reference preselection should be true or not
     chunk.matchPhotos(generic_preselection=True, reference_preselection=False)
     chunk.optimizeCameras() #defaults are ok (match params in gui)
 
     # align cameras
     chunk.alignCameras()
 
-    # todo - optimize cameras
+    #//todo - optimize cameras (?)
 
-    #build depth map - todo not sure about depth value (should be 'medium')
+    #build depth map - //todo not sure about depth value (should be 'medium')
     chunk.buildDepthMaps(downscale=4, filter_mode=Metashape.MildFiltering)
 
     #build dense cloud
@@ -29,12 +35,13 @@ def main():
     #build model/mesh
     chunk.buildModel(surface_type=Metashape.Arbitrary, face_count=Metashape.HighFaceCount, interpolation=Metashape.EnabledInterpolation, vertex_colors=True)
 
-    # todo - what is this? do we need it?
+    # //todo - what is this? do we need it?
     chunk.buildUV(mapping=Metashape.GenericMapping)
 
     #build texture
     chunk.buildTexture(blending=Metashape.MosaicBlending, texture_size=4096,fill_holes=True, ghosting_filter=True)
-    #todo - confirm this step is needed for python 
+
+    #//todo - confirm this step is needed for python 
     chunk.buildOrthomosaic(resolution_x=0.0701037, resolution_y=0.0701037)
     doc.save('result.psz')
 
