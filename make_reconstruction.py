e696c9ddd753eddee10e5bd4bfaad4fe5c632d80
Jennifer Switzer <jfs@mit.edu>
2021-01-18 12:00:12 -0800

    stupid bug fix, more resources for kubes

--- make_reconstruction.py
+++ make_reconstruction.py
@@ -36,13 +36,11 @@ def main():
     #build model/mesh
     chunk.buildModel(surface_type=Metashape.Arbitrary, face_count=Metashape.HighFaceCount, interpolation=Metashape.EnabledInterpolation, vertex_colors=True)
 
-    # //todo - what is this? do we need it?
-    chunk.buildUV(mapping=Metashape.GenericMapping)
+    chunk.buildUV()
 
     #build texture
-    chunk.buildTexture(blending=Metashape.MosaicBlending, texture_size=4096,fill_holes=True, ghosting_filter=True)
+    chunk.buildTexture()
 
-    #//todo - confirm this step is needed for python 
     chunk.buildOrthomosaic(resolution_x=0.0701037, resolution_y=0.0701037)
     doc.save('result.psz')
 
