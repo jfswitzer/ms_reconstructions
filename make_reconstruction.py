08a61e60fae82f8b2d6f25774549b4a0518ccbff
Jennifer Switzer <jfs@mit.edu>
2021-01-28 12:30:14 -0800

    fix large file

--- make_reconstruction.py
+++ make_reconstruction.py
@@ -4,46 +4,59 @@ Image reconstruction in metashape
 """
 import Metashape
 import glob
+import logging
 
 def main():
-    #make sure that it knows to use gpus
     Metashape.app.gpu_mask = 2 ** (len(Metashape.app.enumGPUDevices())) - 1
-
-    #initialize
+    logging.basicConfig(filename='metashape.log', level=logging.DEBUG)
+    outfile = 'results.psx'
     doc = Metashape.Document()
-    chunk = doc.addChunk()
-
-    #add photos
+    chunk = doc.addChunk() 
     photos = glob.glob('/extvol/images/*.png')
+    logging.debug("START - Adding Files")    
     chunk.addPhotos(photos)
-    
-    #//todo not sure if reference preselection should be true or not
+    doc.save(outfile)
+    logging.debug("DONE - Adding Files")
+    logging.debug("START - Matching Files")        
     chunk.matchPhotos(generic_preselection=True, reference_preselection=False)
-
-    # align cameras
-    chunk.alignCameras()    
-
+    doc.save(outfile)
+    logging.debug("DONE - Matching Files")
+    logging.debug("START - Aligning Files")            
+    chunk.alignCameras()
+    doc.save(outfile)
+    logging.debug("DONE - Aligning Files")
+    logging.debug("START - Optimizing Files")                
     chunk.optimizeCameras() #defaults are ok (match params in gui)
-
-    #//todo - optimize cameras (?)
-
-    #build depth map - //todo not sure about depth value (should be 'medium')
+    doc.save(outfile)    
+    logging.debug("DONE - Optimizing Files")
+    logging.debug("START - Resetting region")                    
+    chunk.resetRegion()
+    doc.save(outfile)
+    logging.debug("DONE - Resetting region")                        
+    logging.debug("START - Building depth maps")                    
     chunk.buildDepthMaps(downscale=4, filter_mode=Metashape.MildFiltering)
-
-    #build dense cloud
-    chunk.buildDenseCloud()
-
-    #build model/mesh
+    doc.save(outfile)
+    logging.debug("DONE - Building depth maps")
+    logging.debug("START - Building dense cloud")                        
+    chunk.buildDenseCloud() #todo forum point?
+    doc.save(outfile)
+    logging.debug("DONE - Building dense cloud")
+    logging.debug("START - Building model")                            
     chunk.buildModel(surface_type=Metashape.Arbitrary, face_count=Metashape.HighFaceCount, interpolation=Metashape.EnabledInterpolation, vertex_colors=True)
-
+    doc.save(outfile)
+    logging.debug("DONE - Building model")
+    logging.debug("START - Building UV")                                
     chunk.buildUV()
-
-    #build texture
+    doc.save(outfile)
+    logging.debug("DONE - Building UV")
+    logging.debug("START - Building texture")                                    
     chunk.buildTexture()
-
+    doc.save(outfile)
+    logging.debug("DONE - Building texture")
+    logging.debug("START - Building orthomosaic")                                        
     chunk.buildOrthomosaic(resolution_x=0.0701037, resolution_y=0.0701037)
-    doc.save('result.psz')
-
+    doc.save(outfile)
+    logging.debug("DONE - Building orthomosaic")                                        
 
 if __name__=='__main__':
     main()
