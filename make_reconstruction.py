91a1d90228439f209ad3b20f95c24105195cc2d0
Jennifer Switzer <jfs@mit.edu>
2021-02-02 10:53:28 -0800

    save bug fix pt 2

--- make_reconstruction.py
+++ make_reconstruction.py
@@ -17,46 +17,57 @@ def main():
     photos = glob.glob('/extvol/images/*.png')
     logging.debug("START - Adding Files")    
     chunk.addPhotos(photos)
+    doc.read_only = False
     doc.save(outfile)
     logging.debug("DONE - Adding Files")
     logging.debug("START - Matching Files")        
     chunk.matchPhotos(generic_preselection=True, reference_preselection=False)
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Matching Files")
     logging.debug("START - Aligning Files")            
     chunk.alignCameras()
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Aligning Files")
     logging.debug("START - Optimizing Files")                
     chunk.optimizeCameras() #defaults are ok (match params in gui)
+    doc.read_only = False    
     doc.save(outfile)    
     logging.debug("DONE - Optimizing Files")
     logging.debug("START - Resetting region")                    
     chunk.resetRegion()
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Resetting region")                        
     logging.debug("START - Building depth maps")                    
     chunk.buildDepthMaps(downscale=4, filter_mode=Metashape.MildFiltering)
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building depth maps")
     logging.debug("START - Building dense cloud")                        
     chunk.buildDenseCloud() #todo forum point?
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building dense cloud")
     logging.debug("START - Building model")                            
     chunk.buildModel(surface_type=Metashape.Arbitrary, face_count=Metashape.HighFaceCount, interpolation=Metashape.EnabledInterpolation, vertex_colors=True)
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building model")
     logging.debug("START - Building UV")                                
     chunk.buildUV()
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building UV")
     logging.debug("START - Building texture")                                    
     chunk.buildTexture()
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building texture")
     logging.debug("START - Building orthomosaic")                                        
     chunk.buildOrthomosaic(resolution_x=0.0701037, resolution_y=0.0701037)
+    doc.read_only = False    
     doc.save(outfile)
     logging.debug("DONE - Building orthomosaic")                                        
 
